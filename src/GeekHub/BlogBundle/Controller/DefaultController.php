312587e1b92eceafa128cca8f8fabb4f5cabf118
Bohdan Moshchenko <bohdan.moshchenko@gmail.com>
2016-12-05 21:35:23 +0200

    twig: page inheritance

--- src/GeekHub/BlogBundle/Controller/DefaultController.php
+++ src/GeekHub/BlogBundle/Controller/DefaultController.php
@@ -3,11 +3,48 @@
 namespace GeekHub\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    /**
+     * @return Response
+     */
     public function indexAction()
     {
         return $this->render('GeekHubBlogBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @return Response
+     */
+    public function controlStructuresAction()
+    {
+        $mass = [
+            1 => 'one',
+            2 => 'two' ,
+            3 => 'three',
+            4 => 'four',
+            5 => 'five',
+        ];
+
+        return $this->render('@GeekHubBlog/Default/controlStructures.html.twig', [
+            'mass' => $mass,
+        ]);
+    }
+
+    /**
+     * @return Response
+     */
+    public function inheritanceAction()
+    {
+        return $this->render('@GeekHubBlog/Default/inheritance.html.twig');
+    }
+
+    public function secondInheritanceAction()
+    {
+        return $this->render('@GeekHubBlog/Default/secondInheritance.html.twig', [
+            'text' => 'some footer',
+        ]);
     }
 }
